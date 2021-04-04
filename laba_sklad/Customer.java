fe19fc095809b8679d9c0678ef88cb65fef83443
bag-huyag <64858585+bag-huyag@users.noreply.github.com>
2021-04-04 23:07:09 +0300

    Add files via upload

--- laba_sklad/Customer.java
+++ laba_sklad/Customer.java
@@ -1,27 +1,32 @@
 package com.company;
+
 import java.util.Random;
-public class Customer implements Runnable
-{
+
+public class Customer implements Runnable {
     private Sklad st;
-    Customer(Sklad s)//Запуск потребителей
+
+    // Запуск потребителей
+    Customer(Sklad s)
     {
-        st=s;
-        Thread customer=new Thread (this);
+        st = s;
+        Thread customer = new Thread(this);
         customer.start();
     }
-    public void run()//Работа Потребителя
+
+    // Работа Потребителя
+    public void run()
     {
-        Random R=new Random();
+        Random R = new Random();
         try {
-            for (int i=0; i<9; i++){
-                int k=R.nextInt(st.razmer()-1)+1;
-                for (int j=0; j<k; j++){
+            for (int i = 0; i < 9; i++) {
+                int k = R.nextInt(st.razmer() - 1) + 1;
+                for (int j = 0; j < k; j++) {
                     st.get();
                 }
                 Thread.sleep(200);
             }
+        } catch (Exception e) {
         }
-        catch(Exception e){}
     }
 }
 
--- laba_sklad/Main.java
+++ laba_sklad/Main.java
@@ -1,12 +1,18 @@
 package com.company;
 
-public class Main
-{
-    public static void main (String[] args)
-    {
-        Sklad q=new Sklad();//Создаём склад
-        Producer p=new Producer(q);//Запускаем производителей
-        Customer cus1=new Customer(q);//Запускаем 1 потребителя
-        Customer cus2=new Customer(q);//Запускаем 2 потребителя
+public class Main {
+    public static void main(String[] args) {
+
+        // Создаём склад
+        Sklad q = new Sklad();
+
+        // Запускаем производителей
+        Producer p = new Producer(q);
+
+        // Запускаем 1 потребителя
+        Customer cus1 = new Customer(q);
+
+        // Запускаем 2 потребителя
+        Customer cus2 = new Customer(q);
     }
 }
